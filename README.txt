28b67110e067461331466c68c9861dda88b8e253
Nairuz <nairuz.abulhul@gmail.com>
2016-01-23 21:58:57 -0800

    Official Portfolio

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/ZeeMax/pen/eJVJNr.
+
+ This is my official portfolio, a Free Code Camp Project 
---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function(){
+  $('.about').css('filter','none').end();
+  $('.side-contact').delay(1000).animate({
+    left: '+=40px'
+  },'slow');
+});
